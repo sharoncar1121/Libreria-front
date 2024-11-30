54262ba2d989bb0d533b411dac4ec037061b77b1
sharoncar1121 <109883948+sharoncar1121@users.noreply.github.com>
2024-11-30 00:11:47 -0600

    notif

--- components/botones/BtnAlquilar.tsx
+++ components/botones/BtnAlquilar.tsx
@@ -1,5 +1,5 @@
 'use client';
-import React from 'react';
+import React, { useState } from 'react';
 import { useContextAlq } from '@/context/ProviderAlqu';
 import { Libros } from '@/models/libros';
 
@@ -7,17 +7,29 @@ import { Libros } from '@/models/libros';
 
 export default function BtnAlquilar( libroAlq: Libros) {
   const { setLibroAlqu } = useContextAlq(); 
+  const [mensaje, setMensaje] = useState<string | null>(null);
+
 
   const handleAlquilar = () => {
     setLibroAlqu( libroAlq.Id_libro, 2); 
+    setMensaje(`¡Has alquilado el libro "${libroAlq.Nombre_libro}", con ISBN:  "${libroAlq.ISBN}" con éxito!`);
+    setTimeout(() => setMensaje(null), 5000);
   };
 
   return (
-    <button
-      onClick={handleAlquilar}
-      className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600"
-    >
-      Alquilar
-    </button>
+    <div>
+      {mensaje && (
+        <div className="alert alert-success" role="alert">
+          {mensaje}
+        </div>
+      )}
+
+      <button
+        onClick={handleAlquilar}
+        className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600"
+      >
+        Alquilar
+      </button>
+    </div>
   );
 }
