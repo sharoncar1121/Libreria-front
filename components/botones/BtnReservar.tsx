e747d5b10fa6fe5919ed9b67055b73d5f61cfbd4
sharoncar1121 <109883948+sharoncar1121@users.noreply.github.com>
2024-11-30 00:18:42 -0600

    notif espera

--- components/botones/BtnReservar.tsx
+++ components/botones/BtnReservar.tsx
@@ -1,22 +1,33 @@
 'use client';
-import React from 'react';
+import React, { useState } from 'react';
 import { useContextAlq } from '@/context/ProviderAlqu';
 import { Libros } from '@/models/libros';
 
 
 export default function BtnReservar(libroAlq: Libros) {
   const { setLibroEspera } = useContextAlq(); 
+  const [mensaje, setMensaje] = useState<string | null>(null);
 
-  const handleAlquilar = () => {
+  const handleEsperar = () => {
     setLibroEspera( libroAlq.Id_libro, true); 
+    setMensaje(`¡Has pues en espera "${libroAlq.Nombre_libro}" con éxito, se te notificará al estar disponible para que puedas alquilarlo!`);
+    setTimeout(() => setMensaje(null), 5000);
   };
 
   return (
+<div>
+{mensaje && (
+      <div className="alert alert-success" role="alert">
+        {mensaje}
+      </div>
+    )}
     <button
-      onClick={handleAlquilar}
+      onClick={handleEsperar}
       className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600"
     >
       Esperar
     </button>
-  );
+</div>
+    
+  )
 }
